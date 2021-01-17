10a61c163842dd7e3e7bf6dd78c5a37927dd3320
Albymens <albertamensah.na@gmail.com>
2021-01-17 17:49:56 +0000

    changes made

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
  <?php  
-// var_dump($_POST);
+// var_dump($_FILES);
 // exit;
 
      $userName = '';
@@ -27,7 +27,7 @@
     if (isset($_POST)) {
         if( isset($_POST['userName']) && isset($_POST['phoneNumber']) &&
         isset($_POST['email']) && isset($_POST['locationAddress']) && 
-        isset($_POST['region']) && isset($_POST['profilePicture']) ){
+        isset($_POST['region']) && isset($_FILES['profilePicture']) ){
 
         $userName = $_POST["userName"];
         $phoneNumber = $_POST["phoneNumber"];
